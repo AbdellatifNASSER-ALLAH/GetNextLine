5ccc6602923722ffbdfc8a7a6f3d7c6c960ae973
Abdellatif Nasser-Allah <nasserallahabdellatif2@gmail.com>
2024-11-16 20:37:41 +0100

    tested not working :

--- Project/get_next_line.c
+++ Project/get_next_line.c
@@ -6,10 +6,11 @@
 /*   By: abdnasse <abdnasse@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/15 15:50:09 by abdnasse          #+#    #+#             */
-/*   Updated: 2024/11/16 18:35:09 by abdnasse         ###   ########.fr       */
+/*   Updated: 2024/11/16 20:33:40 by abdnasse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "get_next_line.h"
+#include <stdio.h>
 
 char	*get_next_line(int fd)
 {
@@ -21,7 +22,7 @@ char	*get_next_line(int fd)
 	if (!buff)
 		return (NULL);
 	i = 0;
-	while ((bytes = read(fd, buff + i, 1)))
+	while ((bytes = read(fd, buff + i, 1)) > 0)
 	{
 		if (buff[i] == '\n')
 		{
@@ -29,6 +30,11 @@ char	*get_next_line(int fd)
 			break ;
 		}
 		i++;
+	}
+	if (bytes < 0)
+	{
+		free(buff);
+		return (NULL);
 	}
 	buff[i] = '\0';
 	return (buff);
--- Test/test.c
+++ Test/test.c
@@ -1,13 +1,35 @@
 #include "../Project/get_next_line.h"
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include <fcntl.h>
+
+void print(char *str)
+{
+	int i = 0;
+	while(str[i])
+	{
+		if(str[i] == '\n')
+			write(1,"0",1);
+		else if(str[i] == '\0')
+			write(1,"$",1);
+		else
+			write(1, str + i, 1);
+		i++;
+	}
+	free(str);
+	printf("\n");
+}
 
 int main()
 {
 	int fd = open("a", O_RDONLY);
 
-	char *s = get_next_line(fd);
-
-	printf("%s", s);
+	print(get_next_line(fd));
+	print(get_next_line(fd));
+	print(get_next_line(fd));
+	print(get_next_line(fd));
+	print(get_next_line(fd));
+	print(get_next_line(fd));
+	print(get_next_line(fd));
 }
