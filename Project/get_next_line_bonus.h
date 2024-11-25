37426fe969caf34b077b17f7e00abe3360043219
Abdellatif Nasser-Allah <abdnasse@c3r4p9.1337.ma>
2024-11-25 15:05:24 +0100

    all good need to fix the norminette function r more than 25 lines

--- Project/get_next_line_bonus.h
+++ Project/get_next_line_bonus.h
@@ -6,19 +6,19 @@
 /*   By: abdnasse <abdnasse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/15 15:45:38 by abdnasse          #+#    #+#             */
-/*   Updated: 2024/11/24 15:51:41 by abdnasse         ###   ########.fr       */
+/*   Updated: 2024/11/25 14:34:14 by abdnasse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 
 # include <stdlib.h>
 # include <unistd.h>
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 1337
-#endif
+# endif
 # define MAX_FDS 1024
 
 char	*get_next_line(int fd);
