6d3884fc5ddce68ad78593adda2d1c593eebefc0
Abdellatif Nasser-Allah <abdnasse@c3r8p9.1337.ma>
2024-11-30 10:57:49 +0100

    just some tests

--- Test/bonus.c
+++ Test/bonus.c
@@ -8,19 +8,27 @@
 int main() {
 	int fd1 = open("file1", O_RDONLY);
 	int fd2 = open("file2", O_RDONLY);
-	char *line1, *line2;
+	int fd3 = open("file3", O_RDONLY);
+	char *line1, *line2, *line3;
 
 	line1 = get_next_line(fd1);
 	line2 = get_next_line(fd2);
+	line3 = get_next_line(fd3);
 
-	while (line1 || line2) {
+	while (line1 || line2 || line3) {
 		if (line1) {
-			printf("File 1: %s", line1);
+			printf("%s", line1);
 			free(line1);
 			line1 = get_next_line(fd1);
 		}
+		if (line3)
+		{
+			printf("%s", line3);
+			free(line3);
+			line3 = get_next_line(fd3);
+		}
 		if (line2) {
-			printf("File 2: %s", line2);
+			printf("%s", line2);
 			free(line2);
 			line2 = get_next_line(fd2);
 		}
